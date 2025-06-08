9de58ad39d57644d033cfd00131b3ae016cceb9e
sajad salmanipour <sajadsalmanipour@gmail.com>
2025-06-07 20:00:16 -0400

    orig files added

---
+++ examples/build-gcc-release/CMakeFiles/000-Getting_started_01-api_creation.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/factiondev/projects/Kinova-kortex2_Gen3_G3L/api_cpp/examples/000-Getting_started/01-api_creation.cpp" "CMakeFiles/000-Getting_started_01-api_creation.dir/000-Getting_started/01-api_creation.cpp.o" "gcc" "CMakeFiles/000-Getting_started_01-api_creation.dir/000-Getting_started/01-api_creation.cpp.o.d"
+  "/home/factiondev/projects/Kinova-kortex2_Gen3_G3L/api_cpp/examples/utilities.cpp" "CMakeFiles/000-Getting_started_01-api_creation.dir/utilities.cpp.o" "gcc" "CMakeFiles/000-Getting_started_01-api_creation.dir/utilities.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ examples/build-gcc-release/CMakeFiles/102-High_level_movement_02-sequence.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/factiondev/projects/Kinova-kortex2_Gen3_G3L/api_cpp/examples/102-High_level_movement/02-sequence.cpp" "CMakeFiles/102-High_level_movement_02-sequence.dir/102-High_level_movement/02-sequence.cpp.o" "gcc" "CMakeFiles/102-High_level_movement_02-sequence.dir/102-High_level_movement/02-sequence.cpp.o.d"
+  "/home/factiondev/projects/Kinova-kortex2_Gen3_G3L/api_cpp/examples/utilities.cpp" "CMakeFiles/102-High_level_movement_02-sequence.dir/utilities.cpp.o" "gcc" "CMakeFiles/102-High_level_movement_02-sequence.dir/utilities.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
